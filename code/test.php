10a08a44bac615aa4df60a31c419c6b1b60974f7
fcp3 <fcp3@njit.edu>
2018-02-08 16:16:04 -0500

    Added test code for pokephp

---
+++ code/test.php
@@ -0,0 +1,38 @@
+<?php
+
+//Need autoload file to access PokePHP\PokeApi
+require 'vendor/autoload.php';
+use PokePHP\PokeApi;
+
+//$api is an object used to reference everything in PokeApi
+$api = new PokeApi;
+
+//testing the $api object by referencing cheri berry (see PokePHP documentation)
+//using '$api->*(_)' will return a json string, so use json_decode() to transform into an object
+$cheri = json_decode($api->berry(1));
+print($cheri->name);
+//offset variable to increment calls to PokeAPI (scrolls through paginated responses)
+$offset = 0;
+
+while($offset<140){
+	print("Pokemon "."$offset".PHP_EOL);
+	//makes a call to pokeAPI for 20 pokemon
+	$pokes = json_decode($api->resourceList('pokemon', '20', $offset));
+	//parsing through each pokemon the above line above returns
+	foreach($pokes->results as $poke){
+		$name = $poke->name;
+		$typetxt = "";
+		
+		$pokedata = json_decode($api->pokemon($name));
+		$typedata = $pokedata->types;
+		foreach($typedata as $singleType){
+			$typetxt .= $singleType->type->name;
+		}
+		print("Pokemon: ".$name.", Type: ".$typetxt.PHP_EOL);
+	}
+	$offset += 20;
+}
+
+print("Berry Test: ".$cheri->item->name.PHP_EOL);
+
+?>
