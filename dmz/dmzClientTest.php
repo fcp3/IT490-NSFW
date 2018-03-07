80dbfe54b52b216e374fcbbf7f4e9e51164c48fc
fcp3 <fcp3@njit.edu>
2018-03-07 18:21:49 -0500

    Fully implemented pokemon search function in listener and base for webserver script

--- dmz/dmzClientTest.php
+++ dmz/dmzClientTest.php
@@ -6,9 +6,20 @@ require_once('../rabbitMQLib.inc');
 
 $client = new rabbitMQClient("../testRabbitMQ.ini","queryServer");
 
-$request = "This is a test request over queryqueue\n";
+//$request = "This is a test request over queryqueue\n";
+
+$reqType = "pokeSearch";
+
+switch($reqType) {
+	case "pokeSearch":
+		$request["type"] = $reqType;
+		$request["game"] = "red-blue-yellow";
+		//$request["name"] = "bulbasaur";
+		$request["type1"] = "grass";
+		$request["type2"] = "poison";
+}
 
 $response = $client->send_request($request);
-echo $response;
+echo var_dump(json_decode($response));
 
 ?>
--- dmz/dmzListener.php
+++ dmz/dmzListener.php
@@ -7,7 +7,7 @@ require_once('../rabbitMQLib.inc');
 function requestProcessor($request) {
 
 	echo "Found request!" . PHP_EOL;
-	echo $request . PHP_EOL;
+	//echo $request . PHP_EOL;
 	//return "Request was received!\n";
 	
 	$conn = mysqli_connect("localhost", "root", "1234", "SlowPokeBase");
@@ -21,18 +21,21 @@ function requestProcessor($request) {
 	switch($request["type"]){
 		
 		case "pokeSearch":
-
+			echo "running pokeSearch funtion\n";
 			$pokeGame = $request["game"];
 			$pokeArr = array();
-
+			echo "GameID: " . $pokeGame . PHP_EOL;
 			if(isset($request["name"])){
 				$pokeName = $request["name"];
-				
+				echo $pokeName . PHP_EOL;
 				if($query = mysqli_prepare($conn, "SELECT Name, type_1, type_2, attack, defense, sp_att, sp_def, speed, hp, sprite, level  FROM Pokemon WHERE gameID = ? AND Name = ?")) {
 					$query->bind_param("ss", $pokeGame, $pokeName);
+					$query->execute();
 					$query->bind_result($name, $t1, $t2, $att, $def, $spAtt, $spDef, $spd, $hp, $sprite, $lvl);
 
 					while($query->fetch()) {
+						echo var_dump($name);
+						echo var_dump($t1);
 						$pokemon = array();
 						$pokemon["name"] = htmlspecialchars($name);
 						$pokemon["type1"] = htmlspecialchars($t1);
@@ -45,16 +48,21 @@ function requestProcessor($request) {
 						$pokemon["hp"] = htmlspecialchars($hp);
 						$pokemon["level"] = htmlspecialchars($lvl);
 						$pokemon["sprite"] = htmlspecialchars($sprite);
+						//echo var_dump($pokemon) . PHP_EOL;
 						array_push($pokeArr, $pokemon);
 					}	
+				} else {
+					echo "ERROR HERE IN QUERYING POKEMON BY NAME\n";
 				}
 
 			} else {
+				echo "running pokeSearch for pokemon types\n";
 				$pokeType1 = $request["type1"];
 				$pokeType2 = $request["type2"];
 
 				if($query = mysqli_prepare($conn, "SELECT Name, type_1, type_2, attack, defense, sp_att, sp_def, speed, hp, sprite, level  FROM Pokemon WHERE gameID = ? AND (type_1 = ? OR type_1 = ? OR type_2 = ? OR type_2 = ?)")) {
-					$query->bind_param("ssss", $pokeType1, $pokeType2, $pokeType1, $pokeType2)
+					$query->bind_param("sssss", $pokeGame, $pokeType1, $pokeType2, $pokeType1, $pokeType2);
+					$query->execute();
 					$query->bind_result($name, $t1, $t2, $att, $def, $spAtt, $spDef, $spd, $hp, $sprite, $lvl);
 
 					while($query->fetch()) {
@@ -72,8 +80,12 @@ function requestProcessor($request) {
 						$pokemon["sprite"] = htmlspecialchars($sprite);
 						array_push($pokeArr, $pokemon);
 					}
+				} else {
+					echo "ERROR RUNNING POKESEARCH BY TYPE\n";
 				}
-			}			
+			}
+			//echo var_dump($pokeArr);
+			return json_encode($pokeArr);			
 			break;
 	}
 
