6a8ad3ac14509c9cf74a2d7586b704a8f4c07ca3
fcp3 <fcp3@njit.edu>
2018-03-07 17:34:17 -0500

    Finished pokemon search query function

--- dmz/dmzListener.php
+++ dmz/dmzListener.php
@@ -22,11 +22,11 @@ function requestProcessor($request) {
 		
 		case "pokeSearch":
 
+			$pokeGame = $request["game"];
 			$pokeArr = array();
 
 			if(isset($request["name"])){
 				$pokeName = $request["name"];
-				$pokeGame = $request["game"];
 				
 				if($query = mysqli_prepare($conn, "SELECT Name, type_1, type_2, attack, defense, sp_att, sp_def, speed, hp, sprite, level  FROM Pokemon WHERE gameID = ? AND Name = ?")) {
 					$query->bind_param("ss", $pokeGame, $pokeName);
@@ -49,6 +49,30 @@ function requestProcessor($request) {
 					}	
 				}
 
+			} else {
+				$pokeType1 = $request["type1"];
+				$pokeType2 = $request["type2"];
+
+				if($query = mysqli_prepare($conn, "SELECT Name, type_1, type_2, attack, defense, sp_att, sp_def, speed, hp, sprite, level  FROM Pokemon WHERE gameID = ? AND (type_1 = ? OR type_1 = ? OR type_2 = ? OR type_2 = ?)")) {
+					$query->bind_param("ssss", $pokeType1, $pokeType2, $pokeType1, $pokeType2)
+					$query->bind_result($name, $t1, $t2, $att, $def, $spAtt, $spDef, $spd, $hp, $sprite, $lvl);
+
+					while($query->fetch()) {
+						$pokemon = array();
+						$pokemon["name"] = htmlspecialchars($name);
+						$pokemon["type1"] = htmlspecialchars($t1);
+						$pokemon["type2"] = htmlspecialchars($t2);
+						$pokemon["att"] = htmlspecialchars($att);
+						$pokemon["def"] = htmlspecialchars($def);
+						$pokemon["spAtt"] = htmlspecialchars($spAtt);
+						$pokemon["spDef"] = htmlspecialchars($spDef);
+						$pokemon["speed"] = htmlspecialchars($spd);
+						$pokemon["hp"] = htmlspecialchars($hp);
+						$pokemon["level"] = htmlspecialchars($lvl);
+						$pokemon["sprite"] = htmlspecialchars($sprite);
+						array_push($pokeArr, $pokemon);
+					}
+				}
 			}			
 			break;
 	}
